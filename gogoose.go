6478ae81d699a30f3dcb6ee36f1e569a4c036cdb
Chinmay Chaudhari <chinmay@Chinmays-MacBook-Air.local>
2023-10-18 09:19:39 +0530

    bug fix

--- gogoose.go
+++ gogoose.go
@@ -15,7 +15,7 @@ func New(localHostUrl string) *GooseClient {
 }
 
 func (gc *GooseClient) All() (*http.Response, error) {
-	resp, err := http.Get(gc.url)
+	resp, err := http.Get(gc.url + "all")
 	if err != nil {
 		return nil, err
 	}
@@ -28,7 +28,7 @@ func (gc *GooseClient) Send(jsonData interface{}) (*http.Response, error) {
 		return nil, err
 	}
 
-	resp, err := http.Post(gc.url, "application/json", bytes.NewBuffer(jsonBytes))
+	resp, err := http.Post(gc.url+"send", "application/json", bytes.NewBuffer(jsonBytes))
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +42,7 @@ func (gc *GooseClient) Update(jsonData interface{}) (*http.Response, error) {
 		return nil, err
 	}
 
-	resp, err := http.Post(gc.url, "application/json", bytes.NewBuffer(jsonBytes))
+	resp, err := http.Post(gc.url+"update", "application/json", bytes.NewBuffer(jsonBytes))
 	if err != nil {
 		return nil, err
 	}
@@ -56,7 +56,7 @@ func (gc *GooseClient) Delete(jsonData interface{}) (*http.Response, error) {
 		return nil, err
 	}
 
-	resp, err := http.Post(gc.url, "application/json", bytes.NewBuffer(jsonBytes))
+	resp, err := http.Post(gc.url+"delete", "application/json", bytes.NewBuffer(jsonBytes))
 	if err != nil {
 		return nil, err
 	}
